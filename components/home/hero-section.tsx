7e2978fa90904023c79af1faa06f4e8e6e8fd37d
signordemola <adedamola4678@gmail.com>
2025-07-28 14:33:13 -0500

    Home page updated

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -6,6 +6,7 @@ const HeroSection = () => {
     display: "block",
     height: "100%",
     width: "100%",
+    background: "transparent",
   };
 
   const tradingViewCopyrightStyle = `
@@ -124,12 +125,11 @@ const HeroSection = () => {
                   style={{ width: "100%", height: "100%" }}
                 >
                   <iframe
-                    allowtransparency='true'
                     style={tradingViewStyle}
                     src="https://www.tradingview-widget.com/embed-widget/market-overview/?locale=en#%7B%22colorTheme%22%3A%22dark%22%2C%22dateRange%22%3A%2212M%22%2C%22showChart%22%3Atrue%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22largeChartUrl%22%3A%22%22%2C%22isTransparent%22%3Atrue%2C%22showSymbolLogo%22%3Atrue%2C%22showFloatingTooltip%22%3Atrue%2C%22plotLineColorGrowing%22%3A%22rgba(255%2C%20255%2C%20255%2C%201)%22%2C%22plotLineColorFalling%22%3A%22rgba(255%2C%20255%2C%20255%2C%201)%22%2C%22gridLineColor%22%3A%22rgba(255%2C%20255%2C%20255%2C%200.1)%22%2C%22scaleFontColor%22%3A%22rgba(255%2C%20255%2C%20255%2C%200.5)%22%2C%22belowLineFillColorGrowing%22%3A%22rgba(41%2C%2098%2C%20255%2C%200.12)%22%2C%22belowLineFillColorFalling%22%3A%22rgba(41%2C%2098%2C%20255%2C%200.12)%22%2C%22belowLineFillColorGrowingBottom%22%3A%22rgba(41%2C%2098%2C%20255%2C%200)%22%2C%22belowLineFillColorFallingBottom%22%3A%22rgba(41%2C%2098%2C%20255%2C%200)%22%2C%22symbolActiveColor%22%3A%22rgba(41%2C%2098%2C%20255%2C%200.12)%22%2C%22tabs%22%3A%5B%7B%22title%22%3A%22Markets%22%2C%22symbols%22%3A%5B%7B%22s%22%3A%22FOREXCOM%3ASPXUSD%22%2C%22d%22%3A%22S%26P%20500%22%7D%2C%7B%22s%22%3A%22FOREXCOM%3ANSXUSD%22%2C%22d%22%3A%22US%20100%22%7D%2C%7B%22s%22%3A%22FX%3AEURUSD%22%2C%22d%22%3A%22EUR%2FUSD%22%7D%2C%7B%22s%22%3A%22BITSTAMP%3ABTCUSD%22%2C%22d%22%3A%22Bitcoin%22%7D%5D%2C%22originalTitle%22%3A%22Markets%22%7D%5D%2C%22utm_source%22%3A%22www.northstonecu.com%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_campaign%22%3A%22market-overview%22%2C%22page-uri%22%3A%22www.northstonecu.com%2F%22%7D"
                     title="market overview TradingView widget"
                     lang="en"
-                  ></iframe>
+                  />
                   <style
                     dangerouslySetInnerHTML={{
                       __html: tradingViewCopyrightStyle,
